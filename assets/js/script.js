99c79658df24f69922e2682d06bfe0ca9ff1d9c3
Jordan Thomas <jthomas135@hotmail.com>
2022-04-10 10:40:05 -0700

    added javascript with the questions in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,59 @@
-javascript
+let questions = [
+    {
+        qnum: 1,
+        question: "Which of the following is not a data type in javascript?",
+        answer: "Color",
+        choices: [
+            "Number",
+            "String",
+            "Color",
+            "Boolean"
+        ]
+    },
+
+    {
+        qnum: 2,
+        question: "True or False, '===' operator compares only values.",
+        answer: "False",
+        choices: [
+            "True",
+            "False"
+        ]
+    },
+
+    {
+        qnum: 3,
+        question: "What is the index for the first element of an array?",
+        answer: "0",
+        choices: [
+            "1",
+            "0",
+            "start",
+            "3"
+        ]
+    },
+
+    {
+        qnum: 4,
+        question: "How do you write Hello in an alert box?",
+        answer: "alert('Hello');",
+        choices: [
+            "alert=Hello",
+            "alert{Hello}",
+            "text('Hello')",
+            "alert('Hello');"
+        ]
+    },
+
+    {
+        qnum: 5,
+        question: "How do you call a function named 'myFunction' in Javascript?",
+        answer: "myFunction()",
+        choices: [
+            "myFunction()",
+            "call myFunction",
+            "myFunction=",
+            "Hey you! myFunction!",
+        ]
+    }
+];
